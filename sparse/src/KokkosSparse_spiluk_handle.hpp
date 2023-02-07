717f1502a982d68a306304341dea2eca65a3a362
Vinh Dang <vqdang@sandia.gov>
2023-02-06 23:45:00 -0800

    Add printf

--- sparse/src/KokkosSparse_spiluk_handle.hpp
+++ sparse/src/KokkosSparse_spiluk_handle.hpp
@@ -178,6 +178,7 @@ class SPILUKHandle {
     iw = work_view_t(Kokkos::view_alloc(Kokkos::WithoutInitializing, "iw"),
                      nrows_, ncols_);
     Kokkos::deep_copy(iw, nnz_lno_t(-1));
+    printf("In alloc_iw: iw(%d x %d) is at address %p\n", iw.extent(0), iw.extent(1), (void*)(iw.data()));
   }
 
   KOKKOS_INLINE_FUNCTION
